fde95a97b4b4011a215f320c8a94c97e8603eac3
Katie Bankhead <katie@bankheads.net>
2021-10-26 08:35:55 -0600

    Initial Commit

---
+++ PushupTracker/PushupTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PushupTrackerApp.swift
+//  PushupTracker
+//
+//  Created by Katie Bankhead on 10/26/21.
+//
+
+import SwiftUI
+
+@main
+struct PushupTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
